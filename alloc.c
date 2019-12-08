f37ce4ddebc3c42861546f51cb49eab736817c1f
donal <donalshortt@donals.eu>
2019-12-08 05:24:03 +0100

    Several free util functions added

--- alloc.c
+++ alloc.c
@@ -9,37 +9,64 @@ typedef struct metadata_block {
 
 typedef struct free_metadata_block {
   size_t size;
+  void* addr;
+  struct free_metadata_block* next;
   struct free_metadata_block* prev;
 } free_block_t;
 
-free_block_t* head = NULL;
+free_block_t* head = sbrk(8);
+head->next = NULL;
+head->size = -1;
 
 #define WORD 8
 #define WORD_ALIGN (size - (size % WORD) + WORD);
 
-void insert_metadata(size_t size){
+void insert_metadata(size_t size) {
   metadata_block_t* metadata = (metadata_block_t*)sbrk(WORD);
   metadata->size = size;
 }
 
-void* mymalloc(size_t size){
+void remove_block(free_metadata_block_t* block) {
+  block->prev->next = block->next;
+  block->next->prev = block->prev;
+}
+
+void* search_free_blocks(size_t size) {
+  free_metadata_block_t* current = head;
+
+  while (current->next != NULL) {
+    if (current->size >= size) {
+      remove_block(current);
+      return current->addr;
+    }
+    current = current->next;
+  }
+  return NULL;
+}
+
+void* mymalloc(size_t size) {
   insert_metadata(size);
 
-  if (size == 0){
+  if (size == 0) {
     return NULL;
-  } else if ((size % WORD) != 0){
+  } else if ((size % WORD) != 0) {
     size = WORD_ALIGN;
   }
 
-  return sbrk(size);
+  void* free_block = search_free_blocks(size);
+  if (free_block == NULL) {
+    return sbrk(size);
+  }
+
+  return free_block;
 }
 
-void* mycalloc(size_t nmemb, size_t size){
+void* mycalloc(size_t nmemb, size_t size) {
   void* return_pointer = NULL;
 
-  if (nmemb == 0 || size == 0){
+  if (nmemb == 0 || size == 0) {
     return NULL;
-  } else if ((size % WORD) != 0){
+  } else if ((size % WORD) != 0) {
     size = WORD_ALIGN;
   }
 
@@ -49,24 +76,30 @@ void* mycalloc(size_t nmemb, size_t size){
   return return_pointer;
 }
 
-void myfree(void* ptr){
+void myfree(void* ptr) {
   metadata_block_t* block = (ptr - WORD);
   size_t size = block->size;
 
   free_metadata_block_t* current = head;
-  while ()
-  head->
+  while (current->next != NULL) {
+    current = current->next;
+  }
+
+  current->next = sbrk(WORD);
+  current->next->size = size;
+  current->next->addr = ptr;
+  current->next->next = NULL;
+  current->next->prev = current;
 }
 
-void* myrealloc(void *ptr, size_t size){
+void* myrealloc(void *ptr, size_t size) {
   if (ptr == NULL) {
     return NULL;
-  } else if (size){
+  } else if (size) {
     return NULL;
   }
   return NULL;
 }
-
 
 /*
  * Enable the code below to enable system allocator support for your allocator.
